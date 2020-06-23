51268cc8acc7d8ec6ad020764400b7a81e3d17f2
Rafael Barna <rafael@barna.com.br>
2020-06-23 18:11:24 -0300

    Aula 3: Adicionando aleatoriedade aos inimigos

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/time.dart';
+import 'package:flame/flame.dart';
+import 'dart:math';
 
-void main() {
-  runApp(SpaceShooterGame().widget);
+void main() async {
+  Size size = await Flame.util.initialDimensions();
+  runApp(GameWidget(size));
 }
 
 class GameWidget extends StatelessWidget {
+  final Size size;
+
+  GameWidget(this.size);
+
   @override
   Widget build (BuildContext context){
 
-    final game = SpaceShooterGame();
+    final game = SpaceShooterGame(size);
     return GestureDetector (
       onPanUpdate: (DragUpdateDetails details){
         game.onPlayerMove(details.delta);
@@ -26,17 +34,39 @@ Paint _white = Paint()..color = Color(0xFFFFFFFF);
 
 class GameObject {
   Rect position;
+
   void render(Canvas canvas) {
     canvas.drawRect(position, _white);
   }
 }
 
 class SpaceShooterGame extends Game {
-  GameObject player;
 
-  SpaceShooterGame() {
+  final Size screenSize;
+  Random random = Random();
+  static const enemy_speed = 400;
+  GameObject player;
+  Timer enemyCreator;
+
+  List<GameObject> ennemies = [];
+
+  SpaceShooterGame(this.screenSize) {
     player = GameObject()
       ..position = Rect.fromLTWH(100, 100, 50, 50);
+
+      enemyCreator = Timer (1.0, repeat: true, callback: (){
+        enemies.add(
+          GameObject()
+          ..position = Rect.fromLTWH(0,0,50,50)
+        );
+      });
+
+      enemyCreator.start();
+
+      ennemies.add(
+        GameObject()
+        ..position = Rect.fromLTWH((screenSize.width - 50) * random.nextDouble(), 0,50,50);
+      )
   }
 
   void onPlayerMove(Offset delta){
@@ -44,10 +74,23 @@ class SpaceShooterGame extends Game {
   }
 
   @override
-  void update(double dt) {}
+  void update(double dt) {
+    enemyCreator.update(dt);
+
+    enemies.forEach((enemy) {
+      enemy.position = enemy.position.translate(0, enemy_speed * dt)
+    });
+
+    enemies.removeWhere((enemy) => enemy.position.top >= screenSize.height);
+
+  }
 
   @override
   void render(Canvas canvas) {
     player.render(canvas);
+
+    enemies.forEach(ennemy){
+      enemy.render(canvas);
+    });
   }
 }
